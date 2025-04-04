d536ffec8fec67f6d293e511883465df08ebf01d
AfriyanRZ <afriyanraka212@gmail.com>
2025-04-04 15:07:00 +0700

    Menambahkan File Mengecek Angka Positif.py
    📝 Penjelasan:
    Jika user memasukkan angka lebih besar dari 0, maka program akan mencetak bahwa angka tersebut positif.

---
+++ Basics Pemograman Python/Conditional phyton/Program Sederhana/Introduction to the if Statement/Mengecek Angka Positif.py
@@ -0,0 +1,7 @@
+# Program untuk mengecek apakah angka lebih besar dari nol
+angka = int(input("Masukkan sebuah angka: "))
+
+if angka > 0:
+    print("Angka ini adalah positif.") # Kondisi True
+else:
+    print("Angka ini adalah negatif.") # Kondisi False
