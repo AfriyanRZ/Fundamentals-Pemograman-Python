04a5c75c6c3263d758ac80a7459e8da2a7c0e51c
AfriyanRZ <afriyanraka212@gmail.com>
2025-04-04 15:09:32 +0700

    Menambahkan File Mengecek Usia untuk Mengemudi.py
    📝 Penjelasan:
    Jika usia ≥ 17, maka user dianggap layak untuk mengemudi.

---
+++ Basics Pemograman Python/Conditional phyton/Program Sederhana/Introduction to the if Statement/Mengecek Usia untuk Mengemudi.py
@@ -0,0 +1,7 @@
+# Program untuk mengecek apakah user cukup umur untuk mengemudi
+usia = int(input("Masukkan usia Anda: "))
+
+if usia >= 17:
+    print("Anda cukup umur untuk mengemudi.") # True
+    
+
